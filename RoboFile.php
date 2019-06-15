192fe62f6e6e8a86aea70097722c8eded420ef51
Sinevia <info@sinevia.com>
2019-06-15 12:56:10 +0100

    Update RoboFile.php

--- RoboFile.php
+++ RoboFile.php
@@ -9,6 +9,7 @@ class RoboFile extends \Robo\Tasks {
     private $function = 'YOURFUNCTION';
     private $liveUrl = 'https://eu-gb.functions.cloud.ibm.com/api/v1/web/YOURNAMESPACE/default/YOURFUNCTION';
     private $devUrl = 'http://localhost:32222';
+    private $testingFramework = "TESTIFY"; // Options: TESTIFY, PHPUNIT, NONE
     
     /**
      * Installs the serverless framework
@@ -23,12 +24,66 @@ class RoboFile extends \Robo\Tasks {
         }
     }
     
+    function test
+    {
+        if ($this->testingFramework == "TESTIFY") {
+            return $this->testWithTestify();
+        }
+        
+        if ($this->testingFramework == "PHPUNIT") {
+            return $this->testWithPhpUnit();
+        }
+        
+        return true;
+    }
+    
+    /**
+     * Testing with PHPUnit
+     * @url https://phpunit.de/index.html
+     * @return boolean true if tests successful, false otherwise
+     */
+    function testWithPhpUnit()
+    {
+        $this->say('Running PHPUnit tests...');
+        
+        $isSuccessful = $this->taskExec('composer')
+            ->arg('update')
+            ->option('prefer-dist')
+            ->option('optimize-autoloader')
+            ->run()
+            ->wasSuccessful();
+        
+        // 1. Run composer
+        $isSuccessful = $this->taskExec('composer')
+                ->arg('update')
+                ->option('prefer-dist')
+                ->option('optimize-autoloader')
+                ->run()->wasSuccessful();
+        
+        if ($isSuccessful == false) {
+            return false;
+        }
+
+        // 2. Run tests
+        $isSuccessful = $this->taskExec('phpunit')
+                ->dir('vendor/bin')
+                ->option('configuration', '../../phpunit.xml')
+                ->run()
+                ->wasSuccessful();
+                
+        if ($isSuccessful == false) {
+            return false;
+        }
+        
+        return true;
+    }
+    
     /**
      * Testing with Testify
      * @url https://github.com/BafS/Testify.php
      * @return boolean true if tests successful, false otherwise
      */
-    public function testWithTestify()
+    private function testWithTestify()
     {
         $isSuccessful = $this->taskExec('composer')
             ->arg('update')
@@ -66,7 +121,14 @@ class RoboFile extends \Robo\Tasks {
      * Deploys the app to serverless
      */
     function deploy() {
-        // 1. Run composer
+        // 1. Run tests
+        $isSuccessful = $this->test();
+        
+        if ($isSuccessful == false) {
+            return $this->say('Failed');
+        }
+        
+        // 2. Run composer (no-dev)
         $isSuccessful = $this->taskExec('composer')
                 ->arg('update')
                 ->option('prefer-dist')
@@ -75,17 +137,6 @@ class RoboFile extends \Robo\Tasks {
         
         if ($isSuccessful == false) {
             return $this->say('Failed.');
-        }
-
-        // 2. Run tests
-        $isSuccessful = $this->taskExec('phpunit')
-                ->dir('vendor/bin')
-                ->option('configuration', '../../phpunit.xml')
-                ->run()
-                ->wasSuccessful();
-                
-        if ($isSuccessful == false) {
-            return $this->say('Failed');
         }
 
         // 3. Deploy
