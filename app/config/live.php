155c571633133431473b54554e9edfc7ad39ab45
Sinevia <info@sinevia.com>
2019-07-07 07:25:16 +0100

    Create live.php

---
+++ app/config/live.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'URL_BASE' => "https://YOURLIVEURL",
+    "DB_TYPE" => 'mysql',
+    "DB_HOST" => 'localhost',
+    "DB_NAME" => 'DBNAME',
+    "DB_USER" => 'DBUSER',
+    "DB_PASS" => 'DBPASS',
+    "DB_PORT" => '3306',
+];
