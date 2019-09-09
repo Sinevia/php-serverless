40f910b8e1323afff886b924e16bc0cbfb32e67c
Sinevia <info@sinevia.com>
2019-09-09 01:07:10 +0100

    Update staging.php

--- app/config/staging.php
+++ app/config/staging.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'SERVERLESS_FUNCTION_NAME' => '{YOUR_STAGINGSERVERLESS_FUNCTION_NAME}',
+    'SERVERLESS_FUNCTION_NAME' => '{YOUR_STAGING_SERVERLESS_FUNCTION_NAME}',
     'URL_BASE' => "https://{YOUR_STAGING_URL}",
     "DB_TYPE" => 'mysql',
-    "DB_HOST" => 'localhost',
-    "DB_NAME" => 'DBNAME',
-    "DB_USER" => 'DBUSER',
-    "DB_PASS" => 'DBPASS',
+    "DB_HOST" => '{DBHOST}',
+    "DB_NAME" => '{DBNAME}',
+    "DB_USER" => '{DBUSER}',
+    "DB_PASS" => '{DBPASS}',
     "DB_PORT" => '3306',
 ];
