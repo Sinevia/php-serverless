0dba782d080e3d9121f7b94160bfd846a95e33f1
Sinevia <info@sinevia.com>
2019-09-09 01:02:39 +0100

    Create testing.php

---
+++ app/config/testing.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'URL_BASE' => "none",
+    "DB_TYPE" => 'sqlite',
+    "DB_HOST" => ':memory:',
+    "DB_NAME" => ':memory:',
+    "DB_USER" => 'test',
+    "DB_PASS" => '',
+    "DB_PORT" => '',
+];
