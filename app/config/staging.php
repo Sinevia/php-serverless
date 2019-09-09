5ac4fbd7a76a0ae47ee4ecfd0ec5d15f60027471
Sinevia <info@sinevia.com>
2019-09-09 01:05:12 +0100

    Update staging.php

--- app/config/staging.php
+++ app/config/staging.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    'URL_BASE' => "https://YOURSTAGINGURL",
+    'SERVERLESS_FUNCTION_NAME' => '{YOUR_STAGINGSERVERLESS_FUNCTION_NAME}',
+    'URL_BASE' => "https://{YOUR_STAGING_URL}",
     "DB_TYPE" => 'mysql',
     "DB_HOST" => 'localhost',
     "DB_NAME" => 'DBNAME',
