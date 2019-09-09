e366c483441f55223d1dc8b6a612df6c116a8a84
Sinevia <info@sinevia.com>
2019-09-09 01:06:37 +0100

    Update live.php

--- app/config/live.php
+++ app/config/live.php
@@ -1,11 +1,12 @@
 <?php
 
 return [
-    'URL_BASE' => "https://YOURLIVEURL",
+    'SERVERLESS_FUNCTION_NAME' => '{YOUR_LIVE_SERVERLESS_FUNCTION_NAME}',
+    'URL_BASE' => "https://{YOUR_LIVE_URL}",
     "DB_TYPE" => 'mysql',
-    "DB_HOST" => 'localhost',
-    "DB_NAME" => 'DBNAME',
-    "DB_USER" => 'DBUSER',
-    "DB_PASS" => 'DBPASS',
+    "DB_HOST" => '{DBHOST}',
+    "DB_NAME" => '{DBNAME}',
+    "DB_USER" => '{DBUSER}',
+    "DB_PASS" => '{DBPASS}',
     "DB_PORT" => '3306',
 ];
